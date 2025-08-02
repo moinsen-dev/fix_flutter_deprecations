5195549072b112286c4efff060b6582cfcf68ebf
Ulrich Diedrichsen <uli@moinsen.dev>
2025-08-02 07:28:05 +0200

    chore: Project setup

---
+++ lib/fix_flutter_deprecations.dart
@@ -0,0 +1,10 @@
+/// fix_flutter_deprecations, Flutter Deprecation Fixer
+///
+/// ```sh
+/// # activate fix_flutter_deprecations
+/// dart pub global activate fix_flutter_deprecations
+///
+/// # see usage
+/// fix_deprecations --help
+/// ```
+library;
